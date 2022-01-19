ce42ef839633d7dca4129361e25f2cd63538f567
Brandon Lam <b39lam@uwaterloo.ca>
2022-01-19 02:41:26 -0500

    Got prof help, did not include limit (tol) yet nor a check method if the answer is correct

--- Psych 420 Assignmnet 1 Guess & approach derivative.py
+++ Psych 420 Assignmnet 1 Guess & approach derivative.py
@@ -1,30 +1,34 @@
 import math
 
 def functionOriginal(x):
-    f = x**1/4
-    return f
+    return (x ** 4)
+
 
 def functionDerived(x):
-    f_d = 4*x**3
-    return f_d
-#used the power rule
-def originalvalue (x_0):
+    return (4 * x ** 3)
+
+
+# used the power rule
+# You need both a goal and an initial guess
+# Use variable (and function) names that make sense and communicate something
+def originalvalue(goal, guess):
     iteration = 1
     while True:
-        f = functionOriginal(x_0)
-        f_d = functionDerived(x_0)
-        #defining the function allows x to be x_0 within the equation
-
-        x_next = x_0 - (f/f_d)
-        print ("x_{} = {}".format(iteration, x_next))
-        x_0 = x_next
+        # Computing values at current guess
+        f = functionOriginal(guess)
+        f_d = functionDerived(guess)
+        # Misplace parentheses; details matter
+        error = (goal - f) / f_d
+        print("x_{} = {}".format(iteration, guess))
+        guess = error + guess
         # this is the update method, the initial value of x0 gets updated to be x_next
         # x_next uses newton formula of xn+1 = xn -f(xn)/f`(xn)
 
-        iteration = iteration +1
+        iteration = iteration + 1
 
         if iteration == 10:
-            #breaks after n number of the loop running
+            # breaks after n number of the loop running
             break
 
-originalvalue(3)
+
+originalvalue(16, 2.3)
