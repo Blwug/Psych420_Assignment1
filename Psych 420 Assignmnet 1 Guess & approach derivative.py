fa622503e538a0c8f95ec8ecfd732a1ddabc3f5b
Brandon Lam <b39lam@uwaterloo.ca>
2022-01-19 23:27:33 -0500

    resolved the issue, however, I need to make it more readable & lower the tolerance threshold.

--- Psych 420 Assignmnet 1 Guess & approach derivative.py
+++ Psych 420 Assignmnet 1 Guess & approach derivative.py
@@ -1,14 +1,16 @@
 import math
 
 def functionOriginal(x):
-    return (x ** 4)
+    return (x ** n)
+   # return (x ** 4)
 
 def functionDerived(x):
-    return (4 * x ** 3)
+    return (n * x ** (n-1))
+     #return (4 * x ** (4-1))
 
 const_tol = .001
 
-def originalvalue(goal, guess):
+def originalvalue(goal, guess, n):
 
     iteration = 1
 
@@ -21,18 +23,23 @@ def originalvalue(goal, guess):
         guess = error + guess
         iteration = iteration +1
         #this helps the program remember the number of instance the function occured
-        if guess/- error < const_tol:
-            #this function stops a repeating out
+        if ((guess* error)**2)/ abs(guess - error) < const_tol:
+            #this function stops a repeating output &
                 #ie: x_5 = 2, x_6 = 2 will not occur
             break
         elif iteration == 30:
             break
        # after 30 instance of the program, the program stops
 
-#goal = float(input("goal "))
-#guess = float(input("guess "))
-#running line 36 and line 37 returns nothing; however line 40 solves for any nth root, goal, given any initial guess of the root.
+goal = float(input("please enter the goal "))
+guess = float(input("please enter your guess "))
 
-originalvalue(16,5)
+n = float(input("please enter the root you are trying to find "))
 
+originalvalue(goal, guess, n)
 
+#originalvalue (16, 5, 4)
+#commenting lines 4,8, 34,35,37,39 and uncommenting lines 5,9,41
+#gives the output x_6 = 2.0033728398582538
+
+#inputting 16,5,4 gives x_6 = 2.0033728398582538
