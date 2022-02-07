fe453a6b145bd4e7e1a5128bc21c8e5c6fff2e75
Brandon Lam <b39lam@uwaterloo.ca>
2022-02-07 17:40:22 -0500

    tested another method

--- python testing functions.py
+++ python testing functions.py
@@ -1,75 +1,68 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 voltage = 1
 initial_time = 0
 stop_time = 10
 time_step = .5
 cap = 2
 res = 1
-tol = 3
-max = 8
+voltage_tol = 3
+max_voltage = 8
 resting_potential = 0
-current = 4
+current = [4]
 
 injectionCurrent = 4.3
 injectionStartTime = 1
 injectionEndTime = 6
-updated_injection_value = []
-injectionTimes = injectionStartTime, injectionEndTime
-new_acc_value = []
-updated_time = []
-new_voltage = []
-
-def times (initial_time, updated_time): #this stores the new value of time
-    updated_time.append(initial_time)
-
-def acc (cap,res,current, new_voltage): # this stores the new value of acceleratioThe rn, dv/dt
-    updated_acc = (1/(cap*res)) * (res*current - new_voltage) #tau is equal to cap/res
-    new_acc_value.append(updated_acc)
-
-def voltages (voltage, new_acc_value, updated_time):
-    updated_voltage = voltage + new_acc_value [-1] * updated_time [-1]
-    new_voltage.append(updated_voltage)
+function_injection_value = []
+new_dv_dt_value = []
+new_time = []
+new_voltage = [voltage]
 
 
+def times(initial_time, new_time):  # this stores the new value of time
+    new_time.append(initial_time)
 
+
+def dv_dt(cap, res, current, new_voltage):
+    function_dv_dt = (1 / (cap * res)) * (res * current - new_voltage[-1])
+    new_dv_dt_value.append(function_dv_dt)
+
+
+def function_voltage(voltage, new_dv_dt_value, new_time):
+    function_voltage = voltage + new_dv_dt_value[-1] * new_time[-1]
+    new_voltage.append(function_voltage)
 
 
 while initial_time < stop_time:
     initial_time += time_step
-    acc(cap,res,current, voltage)
-    times(initial_time, updated_time)
+    dv_dt(cap, res, current, new_voltage)
+    times(initial_time, new_time)
 
-    voltages(voltage, new_acc_value, updated_time)
-    new_voltage [0] = resting_potential
-    if new_voltage[-1] == tol:
-        new_voltage[-1] = max
-    elif new_voltage[-1] <tol:
-        new_voltage[-1] = tol
-    else: new_voltage [-1] = resting_potential
+    function_voltage(new_voltage, new_dv_dt_value, new_time)
+    new_voltage[0] = resting_potential
+    if new_voltage[-1] == voltage_tol:
+        new_voltage [-1] = max_voltage
+    elif new_voltage[-1] < voltage_tol:
+        new_voltage[-1] = voltage_tol
+    #elif new_voltage[-1] == voltage_tol:
+        #new_voltage[-1] = max_voltage
 
-
-
-
-
-print (new_acc_value)
-print (updated_time)
+print(new_dv_dt_value)
+print(new_time)
 print(new_voltage)
 
-plt.plot (new_voltage,updated_time,)
+plt.plot(new_voltage, new_time, )
 plt.ylabel('voltage')
 plt.xlabel('time')
 plt.show()
 
+# def update_voltage (voltage, ):
 
 
-#def update_voltage (voltage, ):
+# set the value of voltage
+# update the value of voltage using dv/dt
+# then update the value of voltage using old voltage + dv/dt*time_step
 
-
-
-
-#set the value of voltage
-#update the value of voltage using dv/dt
-#then update the value of voltage using old voltage + dv/dt*time_step
-
-#break down the questions to make it tolerable to do
+# break down the questions to make it tolerable to do
