f5c3a6e8eb7e4aac460b143154fd1594519b7bc9
Brandon Lam <b39lam@uwaterloo.ca>
2022-02-08 20:28:08 -0500

    def and an if error

--- python testing functions.py
+++ python testing functions.py
@@ -12,18 +12,21 @@ resting_potential = 0
 current = 4
 
 injectionCurrent = 0
-injectionStartTime = 1
+injectionStartTime = 0
 injectionEndTime = 6
 function_injection_value = []
 new_dv_dt_value = []
 new_time = [0]
 new_voltage = [voltage]
-
-
+new_current = [injectionCurrent]
+new_injectiontime = [injectionCurrent]
 #if the current injection time is true, then have the voltage run else have it fail the next interval of the threshold
 
 def times(initial_time, new_time):  # this stores the new value of time
     new_time.append(initial_time)
+
+def current_injection (injectionStartTime, new_injectiontime):
+        new_injectiontime.append(injectionStartTime)
 
 
 def dv_dt(cap, res, current, new_voltage):
@@ -32,44 +35,46 @@ def dv_dt(cap, res, current, new_voltage):
 
 
 def function_voltage(voltage, new_dv_dt_value, time_step):
-    function_voltage = voltage + new_dv_dt_value[-1] * time_step
+    function_voltage = (voltage + new_dv_dt_value[-1] * time_step)
     new_voltage.append(function_voltage)
 
-def function_current (injectionCurrent, new_time, injectionStartTime, injectionEndTime):
-    if new_time[-1] > injectionStartTime and new_time[-1] < injectionEndTime:
 
 
 #we can do a nested loop.. no
+
+
 while initial_time < stop_time: #and injectionStartTime < injectionEndTime:
-    injectionCurrent = injectionCurrent * new_time[-1] #update the current value
+
     initial_time += time_step
     dv_dt(cap, res, current, new_voltage)
     times(initial_time, new_time)
 
-    #function_voltage(new_voltage[-1], new_dv_dt_value, time_step)
+
+    if injectionStartTime >= injectionEndTime:
+        injectionStartTime = 1
+    #elif injectionStartTime <= injectionEndTime:
+      #      injectionStartTime = 0
+
+    current_injection(injectionStartTime, new_injectiontime)
 
 
     if new_voltage[-1] >=max_voltage:
         new_voltage[-1] = resting_potential
     elif new_voltage[-1] > voltage_tol:
         new_voltage[-1] = max_voltage
-    elif new_voltage [-1] < 0:
-        new_voltage[-1] = 0
-    elif new_time[-1] <2: #this function is the pesudo currentinjection time
+    elif new_time[-1] <2:
       new_voltage[-1] = 0
-    elif new_time[-1] >12:
-      new_voltage[-1] = 0
-    elif new_time[-1] >15:
-        new_voltage[-1] = 0
-
 
     function_voltage(new_voltage[-1], new_dv_dt_value, time_step)
+
+
+
 
 
 print(new_dv_dt_value)
 print(new_time)
 print(new_voltage)
-print(voltage_tol)
+print(new_injectiontime)
 
 plt.plot(new_time, new_voltage)
 plt.xlabel('time')
