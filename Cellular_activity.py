478377f066389cd6eebe0174771304ea362d7d60
Brandon Lam <b39lam@uwaterloo.ca>
2022-03-05 18:23:58 -0500

    done

--- Cellular_activity.py
+++ Cellular_activity.py
@@ -3,22 +3,23 @@ import numpy as np
 x1 = [5]
 x2 = [4]
 x3 = [7]
-bias_1 = [-0.3]
-bias_2 = [0.6]
+weight_1 = [-0.3]
+weight_2 = [0.6]
+bias = 5.0
 
 inner_layer = np.array([x1,x2,x3])
 
-hidden_layer_bias = np.array([bias_1, bias_2]) #print(hidden_layer_bias)
-hidden_layer_bias = np.reshape(hidden_layer_bias, 2) #print(hidden_layer_bias)
+hidden_layer_weight = np.array([weight_1, weight_2]) #print(hidden_layer_bias)
+hidden_layer_weight = np.reshape(hidden_layer_weight, 2) #print(hidden_layer_bias)
 
 
 inner_hidden_dot_product_1 = np.zeros((1,2))
 
-for i in range (len(inner_layer)):
-   for j in range (len(hidden_layer_bias)):
-        inner_hidden_dot_product_1 = inner_layer[i][j] * hidden_layer_bias[j][i]
+#for i in range (len(inner_layer)):
+   #for j in range (len(hidden_layer_weight)):
+        #inner_hidden_dot_product_1 = inner_layer[i][j] * hidden_layer_weight[j][i]
 
-#inner_hidden_dot_product_1 = inner_layer * hidden_layer_bias
+inner_hidden_dot_product_1 = inner_layer * hidden_layer_weight + bias
 
 
 print(inner_hidden_dot_product_1)
