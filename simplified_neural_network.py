cd89294acf9432ddafe35d9697eb3496f8e959a7
Brandon Lam <b39lam@uwaterloo.ca>
2022-03-07 15:04:54 -0500

    simple neural model works

--- simplified_neural_network.py
+++ simplified_neural_network.py
@@ -1,28 +1,30 @@
-import numpy as np #scaler is one value
+import numpy as np  # scaler is one value
 
-inputs = [1.0, 2.0, 3.0, 2.2] #shapes is (1,4)
+inputs = [1.0, 2.0, 3.0, 2.2]  # shapes is (1,4)
 
-weights = [[0.2, 0.8, -0.3, 0.1], #shapes is (3,4) & matrix has to have the same size
-                [0.5, -0.9, -0.2, 0.5], #2D array && a matrix
-                [0.2, -0.8, -0.3, 0.9] ]
+weights = [[0.2, 0.8, -0.3, 0.1],  # shapes is (3,4) & matrix has to have the same size
+           [0.5, -0.9, -0.2, 0.5],  # 2D array && a matrix
+           [0.2, -0.8, -0.3, 0.9]]
 
-biases = [1.0, 2.0, 0.5] #represents the b in ymx + b
+biases = [1.0, 2.0, 0.5]  # represents the b in ymx + b
 
-output = np.dot (weights, inputs) + biases
+output = np.dot(weights, inputs) + biases
 
-threshold = [2,2,2]
+threshold = [2, 2, 2]
 
 for output_i, threshold_i in zip(output, threshold):
     difference = output_i - threshold_i
     break
 
-print ("difference of output - threshold prior to the gate " +str(difference))
+print("difference of output - threshold prior to the gate " + str(difference))
 
-if float(difference) > 0: difference = 1
-if float(difference) < 0: difference = 0
+if float(difference) > 0:
+    difference = 1
+else:
+    if float(difference) < 0: difference = 0
 
-print ("converted value of difference "+ str(difference))
-print (output)
+print("converted value of difference " + str(difference))
+print(output)
 
 '''
 every perceptron can have a different value for their threshold
@@ -33,4 +35,3 @@ more neurons = more area of effects which makes the neuron more representative
 
     
 '''
-
